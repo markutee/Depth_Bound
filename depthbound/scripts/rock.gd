49f72c627c52be87b5772ade4c302e9758f36486
Einghal <eemeli.lehessalo@hotmail.fi>
2026-02-12 10:53:25 +0200

    animointi oreille

--- depthbound/scripts/rock.gd
+++ depthbound/scripts/rock.gd
@@ -1,6 +1,8 @@
 extends StaticBody2D
 class_name Rock
 
+const ORE_SCENE := preload("res://scenes/ore.tscn")
+
 var health: int
 
 
@@ -18,7 +20,26 @@ func take_damage(amount: int) -> void:
 	health -= amount
 	print(health)
 	if health <= 0:
+		_drop_ore()
 		_destroy()
+
 
 func _destroy() -> void:
 	queue_free()
+
+
+func _drop_ore() -> void:
+	var ore = ORE_SCENE.instantiate()
+	ore.position = position
+	ore.ore_data = data.ore_resource
+	
+	var game_root = get_parent().get_parent()
+	var ore_container = game_root.get_node("OreContainer")
+	ore_container.add_child(ore)
+
+	var tween = ore.create_tween()
+	#Vertical bounce
+	tween.tween_property(ore, "position:y", ore.position.y - 20, 0.3)\
+		.set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_QUAD)
+	tween.tween_property(ore, "position:y", ore.position.y, 0.3)\
+		.set_ease(Tween.EASE_IN).set_trans(Tween.TRANS_QUAD)#.set_delay(0.3)
