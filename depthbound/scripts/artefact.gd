ad80924d8dbbf8d5fc6b2b0542266c98a8cb9fda
Einghal <eemeli.lehessalo@hotmail.fi>
2026-04-16 11:46:46 +0300

    valoja...

--- depthbound/scripts/artefact.gd
+++ depthbound/scripts/artefact.gd
@@ -1,7 +1,15 @@
 extends Area2D
 
 @export var artefact_data: ArtefactData
+
 @onready var sprite_2d: Sprite2D = $Sprite2D
+@onready var glow_pivot: Node2D = $GlowPivot
+@onready var rays: Sprite2D = $GlowPivot/Rays
+
+var base_scale := 0.2
+var pulse_speed := 2.5
+var pulse_amount := 0.06
+var pulse_time := 0.0 
 
 func _ready() -> void:
 	if artefact_data == null:
@@ -9,6 +17,18 @@ func _ready() -> void:
 		return
 
 	sprite_2d.texture = artefact_data.texture
+	
+# varmistetaan että rays pyörii keskeltä
+	rays.centered = true
+	rays.offset = Vector2.ZERO
+	rays.position = Vector2.ZERO
+	
+func _process(delta: float) -> void:
+	glow_pivot.rotation += delta * 0.8
+	# Sykkiminen
+	pulse_time += delta
+	var pulse := 1.0 + sin(pulse_time * pulse_speed) * pulse_amount
+	rays.scale = Vector2.ONE * (base_scale * pulse)
 
 func _on_body_entered(body: Node2D) -> void:
 	if artefact_data == null:
